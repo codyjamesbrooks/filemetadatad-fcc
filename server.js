0a6690bd95b6142e0879b99f7b6ccfe2e52a102d
codyjamesbrooks <71521423+codyjamesbrooks@users.noreply.github.com>
2021-05-13 09:09:36 -0600

    finished json res on file upload

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upLoad = multer({ storage: storage });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(process.cwd() + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upLoad.single("upfile"), (req, res, next) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+  next();
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
